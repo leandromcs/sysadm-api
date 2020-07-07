8218bce22affd20ddd5abeb0d39370a7fd70f975
Leandro Mendes <leandromcs96@gmail.com>
2020-07-07 00:24:09 -0300

    Projeto Base

---
+++ src/main/java/com/example/sysadm/SysadmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sysadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysadmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysadmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sysadm/SysadmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sysadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysadmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
